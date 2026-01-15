44c3557fbf5c3bcb5638357d37e532ce95cf28ec
xiayuan770 <326564714@qq.com>
2026-01-15 22:38:26 +0800

    Site updated: 2026-01-15 22:38:25

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1768487891473";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1768487905505";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
